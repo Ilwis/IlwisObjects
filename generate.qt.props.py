7f926573a9966545e0c562331029eeeaff0d1f11
retsios <v.retsios@utwente.nl>
2025-02-20 11:33:20 +0100

    Added Python 3.13 support

--- generate.qt.props.py
+++ generate.qt.props.py
@@ -4,7 +4,7 @@ import sys
 
 qtdir=sys.argv[1]
 pythonsearchpath=sys.argv[2]
-pythonroots={'PYTHONROOT':pythonsearchpath+r'\Python36', 'PYTHONROOT37':pythonsearchpath+r'\Python37', 'PYTHONROOT38':pythonsearchpath+r'\Python38', 'PYTHONROOT39':pythonsearchpath+r'\Python39', 'PYTHONROOT310':pythonsearchpath+r'\Python310', 'PYTHONROOT311':pythonsearchpath+r'\Python311', 'PYTHONROOT312':pythonsearchpath+r'\Python312'}
+pythonroots={'PYTHONROOT':pythonsearchpath+r'\Python36', 'PYTHONROOT37':pythonsearchpath+r'\Python37', 'PYTHONROOT38':pythonsearchpath+r'\Python38', 'PYTHONROOT39':pythonsearchpath+r'\Python39', 'PYTHONROOT310':pythonsearchpath+r'\Python310', 'PYTHONROOT311':pythonsearchpath+r'\Python311', 'PYTHONROOT312':pythonsearchpath+r'\Python312', 'PYTHONROOT313':pythonsearchpath+r'\Python313'}
 
 print('<?xml version="1.0" encoding="utf-8"?>')
 print('<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">')
